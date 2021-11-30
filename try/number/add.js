f81532d640471555711781a5f1ef379e7d7d053f
LlanceCaloza <calozallance@gmail.com>
2021-11-30 20:27:57 +0800

    first commit

---
+++ try/number/add.js
@@ -0,0 +1,128 @@
+const option1 = document.getElementById("option1"),
+      option2 = document.getElementById("option2"),
+      option3 = document.getElementById("option3"),
+      audio = document.getElementById("Audio");  
+      audio2 = document.getElementById("Audio2");  
+var answer = 0;
+
+function generate_equation(){ 
+  var num1 = Math.floor(Math.random() * 13),
+      num2 = Math.floor(Math.random() * 13),
+      dummyAnswer1 = Math.floor(Math.random() * 10),
+      dummyAnswer2 = Math.floor(Math.random() * 10),
+      allAnswers = [],
+      switchAnswers = [];
+
+  answer = eval(num1 + num2);
+  
+  document.getElementById("num1").innerHTML = num1; 
+  document.getElementById("num2").innerHTML = num2; 
+
+  allAnswers = [answer, dummyAnswer1, dummyAnswer2];
+
+  for (i = allAnswers.length; i--;){
+    switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
+  };
+
+  option1.innerHTML = switchAnswers[0];
+  option2.innerHTML = switchAnswers[1];
+  option3.innerHTML = switchAnswers[2]; 
+};
+
+option1.addEventListener("click", function(){
+    if(option1.innerHTML == answer){
+      generate_equation();
+      audio2.play();
+    }
+    else{
+      audio.play();
+      
+    }
+});
+
+option2.addEventListener("click", function(){
+    if(option2.innerHTML == answer){
+      generate_equation();
+      audio2.play();
+    }
+    else{
+      audio.play();
+    }
+});
+
+option3.addEventListener("click", function(){
+    if(option3.innerHTML == answer){
+      generate_equation();
+      audio2.play();
+    }
+    else{
+      audio.play();
+    }
+});
+
+generate_equation()
+
+
+
+
+//test
+function generate_equation1(){ 
+  var num3 = Math.floor(Math.random() * 13),
+      num4 = Math.floor(Math.random() * 13),
+      dummyAnswer1 = Math.floor(Math.random() * 50),
+      dummyAnswer2 = Math.floor(Math.random() * 50),
+      allAnswers = [],
+      switchAnswers = [];
+
+  answer = eval(num3 * num4);
+  
+  document.getElementById("num1").innerHTML = num3; 
+  document.getElementById("num2").innerHTML = num4; 
+
+  allAnswers = [answer, dummyAnswer1, dummyAnswer2];
+
+  for (i = allAnswers.length; i--;){
+    switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
+  };
+
+  option1.innerHTML = switchAnswers[0];
+  option2.innerHTML = switchAnswers[1];
+  option3.innerHTML = switchAnswers[2]; 
+
+};
+
+option1.addEventListener("click", function(){
+    if(option1.innerHTML == answer){
+      generate_equation1();
+      audio2.play();
+    }
+    else{
+      audio.play();
+    }
+});
+
+option2.addEventListener("click", function(){
+    if(option2.innerHTML == answer){
+      generate_equation1();
+      audio2.play();
+    }
+    else{
+      audio.play();
+    }
+});
+
+option3.addEventListener("click", function(){
+    if(option3.innerHTML == answer){
+     generate_equation1();
+     audio2.play();
+    }
+    else{
+      audio.play();
+    }
+});
+
+generate_equation1()
+
+
+
+
